c0db89e06531a6ef5366d83f98417f70704224cd
jressym <jressym@gmail.com>
2025-12-02 16:18:24 +0700

    admin film studio dan membership
    script CRUD API baru

--- app/main.py
+++ app/main.py
@@ -1,27 +1,21 @@
 # app/main.py
 from fastapi import FastAPI
-from app.routers import admin_film, admin_jadwal, user_catalog, user_transaction
+from app.routers import admin_film #, admin_jadwal, user_catalog, user_transaction
 
-description = """
-Sistem ini menyediakan serangkaian endpoint untuk mengelola seluruh alur pemesanan tiket bioskop secara digital, dengan modul utama:
-
-* **Admin**: Mengelola data film, studio, dan jadwal tayang.
-* **User**: Melihat katalog film, memesan tiket, dan melihat riwayat transaksi.
-"""
 
 app = FastAPI(
     title="Movie Booking System",
-    description=description,
-    version="1.2.0"
+    description="Proyek UAS",
+    version="2.1.0"
 )
 
 # Admin
-app.include_router(admin_film.router,  tags=["Admin - Film & Studio"])
-app.include_router(admin_jadwal.router, tags=["Admin - Jadwal"])
+app.include_router(admin_film.router,  tags=["Admin - Film, Studio, dan Memberships"])
+# app.include_router(admin_jadwal.router, tags=["Admin - Jadwal"])
 
 # User
-app.include_router(user_catalog.router,     tags=["User - Katalog"])           
-app.include_router(user_transaction.router, tags=["User - Cart & Checkout"])
+# app.include_router(user_catalog.router,     tags=["User - Katalog"])           
+# app.include_router(user_transaction.router, tags=["User - Cart & Checkout"])
 
 @app.get("/")
 def home():
--- app/routers/admin_film.py
+++ app/routers/admin_film.py
@@ -1,112 +1,149 @@
 from fastapi import APIRouter, HTTPException
-from app.models import Movie, Studio
+from pydantic import BaseModel
+from app.models import Movie, Studio, Membership, Session, engine
 
 router = APIRouter()
+session = Session()
 
-# Database film
-list_film = [{"id": "mov1", "title": "Avengers: Endgame", "duration": "200 menit", "genre": "Action, Fantasy", "sutradara": "Anthony Russo, Joe Russo", "rating_usia": "PG-13", "price": "Rp40.000"},
-{"id": "mov2", "title": "The Conjuring", "duration": "120 menit", "genre": "Horror, Mystery", "sutradara": "James Wan", "rating_usia": "17+", "price": "Rp40.000"},
-{"id": "mov3", "title": "Frozen", "duration": "130 menit", "genre": "Family, Musical", "sutradara": "Jennifer Lee, Chris Buck", "rating_usia": "PG", "price": "Rp40.000"},
-{"id": "mov4", "title": "Komang", "duration": "130 menit", "genre": "Drama, Romance", "sutradara": "Naya Anindita", "rating_usia": "13+", "price": "Rp40.000"}
-]
 
-# =============================================== API FILM ================================================
+# pydantic schemas
+class MovieInput(BaseModel):
+    code: str
+    title: str
+    genre: str
+    durasi: int
+    director: str
+    rating: str
+    price: int
 
-# CREATE - Tambah Film
-@router.post("/movies")
-def tambah_film(movie: Movie):
-    # Cek apakah ID film sudah ada
-    for f in list_film:
-        if f["id"] == movie.id:
-            raise HTTPException(status_code=400, detail="ID film sudah ada")
-    list_film.append(movie.dict())
-    return {"message": "Film berhasil ditambahkan", "data": movie}
+class StudioInput(BaseModel):
+    code: str
+    name: str
+    rows: int
+    cols: int
 
-# READ - Lihat semua film
+class MembershipInput(BaseModel):
+    code: str
+    nama: str
+
+
+# film
 @router.get("/movies")
-def lihat_semua_film():
-    if not list_film:
-        raise HTTPException(status_code=404, detail="Belum ada film yang terdaftar")
-    return {"message": "Daftar semua film berhasil diambil", "data": list_film}
+def get_movies():
+    data = session.query(Movie).all()
+    return data
 
-# READ - Lihat satu film
-@router.get("/movies/{movie_id}")
-def lihat_detail_film(movie_id: str):
-    for film in list_film:
-        if film["id"] == movie_id:
-            return {"message": "Detail film ditemukan", "data": film}
-    raise HTTPException(status_code=404, detail="Film tidak ditemukan")
+@router.post("/movies")
+def add_movie(item: MovieInput):
+    m = Movie(**item.dict())
+    session.add(m)
+    try:
+        session.commit()
+        return {"status": "Movie ditambahkan", "movie": item}
+    except:
+        session.rollback()
+        raise HTTPException(status_code=400, detail="Gagal menambahkan movie (kode mungkin sudah ada)")
 
-# UPDATE - Perbarui film
 @router.put("/movies/{movie_id}")
-def perbarui_film(movie_id: str, movie: Movie):
-    for idx, film in enumerate(list_film):
-        if film["id"] == movie_id:
-            list_film[idx] = movie.dict()
-            return {"message": "Data film berhasil diperbarui", "data": movie}
-    raise HTTPException(status_code=404, detail="Film tidak ditemukan")
+def update_movie(movie_id: int, item: MovieInput):
+    m = session.query(Movie).filter_by(id=movie_id).first()
+    if not m:
+        raise HTTPException(status_code=404, detail="Movie tidak ditemukan")
 
-# DELETE - Hapus film
+    for key, val in item.dict().items():
+        setattr(m, key, val)
+
+    session.commit()
+    return {"status": "Movie berhasil diupdate"}
+
 @router.delete("/movies/{movie_id}")
-def hapus_film(movie_id: str):
-    for idx, film in enumerate(list_film):
-        if film["id"] == movie_id:
-            del list_film[idx]
-            return {"message": f"Film dengan ID {movie_id} berhasil dihapus"}
-    raise HTTPException(status_code=404, detail="Film tidak ditemukan")
+def delete_movie(movie_id: int):
+    m = session.query(Movie).filter_by(id=movie_id).first()
+    if not m:
+        raise HTTPException(status_code=404, detail="Movie tidak ditemukan")
+
+    session.delete(m)
+    session.commit()
+    return {"status": "Movie berhasil dihapus"}
 
 
-# =============================================== API STUDIO ================================================
-
-# Database Studio
-list_studio = [
-    {"id": "st1", "name": "Studio 1", "capacity": 96},
-    {"id": "st2", "name": "Studio 2", "capacity": 96},
-    {"id": "st3", "name": "Studio 3", "capacity": 96},
-    {"id": "st4", "name": "Studio 4", "capacity": 96},
-]
 
 
-# CREATE - Tambah Studio
-@router.post("/studios")
-def tambah_studio(studio: Studio):
-    # Cek apakah ID sudah ada
-    for st in list_studio:
-        if st["id"] == studio.id:
-            raise HTTPException(status_code=400, detail="ID studio sudah ada")
-
-    list_studio.append(studio.dict())
-    return {"message": "Studio berhasil ditambahkan", "data": studio}
-
-# READ - Lihat semua Studio
+# studio
 @router.get("/studios")
-def lihat_semua_studio():
-    if not list_studio:
-        raise HTTPException(status_code=404, detail="Belum ada studio yang terdaftar")
-    return {"message": "Daftar semua studio berhasil diambil", "data": list_studio}
+def get_studios():
+    return session.query(Studio).all()
 
-# READ - Lihat satu Studio
-@router.get("/studios/{studio_id}")
-def lihat_detail_studio(studio_id: str):
-    for st in list_studio:
-        if st["id"] == studio_id:
-            return {"message": "Detail studio ditemukan", "data": st}
-    raise HTTPException(status_code=404, detail="Studio tidak ditemukan")
+@router.post("/studios")
+def add_studio(item: StudioInput):
+    s = Studio(**item.dict())
+    session.add(s)
+    try:
+        session.commit()
+        return {"status": "Studio ditambahkan"}
+    except:
+        session.rollback()
+        raise HTTPException(status_code=400, detail="Gagal menambahkan studio")
 
-# UPDATE - Perbarui Studio
 @router.put("/studios/{studio_id}")
-def perbarui_studio(studio_id: str, studio: Studio):
-    for idx, st in enumerate(list_studio):
-        if st["id"] == studio_id:
-            list_studio[idx] = studio.dict()
-            return {"message": "Data studio berhasil diperbarui", "data": studio}
-    raise HTTPException(status_code=404, detail="Studio tidak ditemukan")
+def update_studio(studio_id: int, item: StudioInput):
+    s = session.query(Studio).filter_by(id=studio_id).first()
+    if not s:
+        raise HTTPException(status_code=404, detail="Studio tidak ditemukan")
 
-# DELETE - Hapus Studio
+    for key, val in item.dict().items():
+        setattr(s, key, val)
+
+    session.commit()
+    return {"status": "Studio berhasil diupdate"}
+
 @router.delete("/studios/{studio_id}")
-def hapus_studio(studio_id: str):
-    for idx, st in enumerate(list_studio):
-        if st["id"] == studio_id:
-            del list_studio[idx]
-            return {"message": f"Studio dengan ID {studio_id} berhasil dihapus"}
-    raise HTTPException(status_code=404, detail="Studio tidak ditemukan")
+def delete_studio(studio_id: int):
+    s = session.query(Studio).filter_by(id=studio_id).first()
+    if not s:
+        raise HTTPException(status_code=404, detail="Studio tidak ditemukan")
+
+    session.delete(s)
+    session.commit()
+    return {"status": "Studio berhasil dihapus"}
+
+
+
+
+# memberships
+@router.get("/memberships")
+def get_memberships():
+    return session.query(Membership).all()
+
+@router.post("/memberships")
+def add_member(item: MembershipInput):
+    m = Membership(**item.dict())
+    session.add(m)
+    try:
+        session.commit()
+        return {"status": "Membership ditambahkan"}
+    except:
+        session.rollback()
+        raise HTTPException(status_code=400, detail="Gagal menambahkan membership")
+
+@router.put("/memberships/{member_id}")
+def update_member(member_id: int, item: MembershipInput):
+    m = session.query(Membership).filter_by(id=member_id).first()
+    if not m:
+        raise HTTPException(status_code=404, detail="Membership tidak ditemukan")
+
+    for key, val in item.dict().items():
+        setattr(m, key, val)
+
+    session.commit()
+    return {"status": "Membership berhasil diupdate"}
+
+@router.delete("/memberships/{member_id}")
+def delete_member(member_id: int):
+    m = session.query(Membership).filter_by(id=member_id).first()
+    if not m:
+        raise HTTPException(status_code=404, detail="Membership tidak ditemukan")
+
+    session.delete(m)
+    session.commit()
+    return {"status": "Membership berhasil dihapus"}
